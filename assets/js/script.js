91adc4237be2e16e4f19086b4e455a8aec2b0d6e
tfavro <wetr9902@gmail.com>
2021-12-26 17:30:29 -0600

    added time blocks and coding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 //variables for creating todays date in header
 var currentDayEl = document.getElementById("currentDay");
 var date = document.createElement("p");
+var todaysDate = moment().format("dddd, MMMM  Do");
+
 
 //Create Todays date and display it on the html
-var todaysDate = moment().format("dddd, MMMM  Do");
-    
-
 date.textContent = todaysDate;
 currentDayEl.appendChild(date);
 console.log(todaysDate);
